1c92d128ce216f8a685b0204b344b4169d3c8156
Adrian Schiller <adrian.schiller@avionte.com>
2019-06-10 22:27:38 -0500

    Really applying that math (still broken, but much better than it was before)

--- TetrisAdvanced/Services/ShapeService.cs
+++ TetrisAdvanced/Services/ShapeService.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using TetrisAdvanced.Data;
 using TetrisAdvanced.Interfaces;
 
@@ -10,11 +8,11 @@ namespace TetrisAdvanced.Services
     {
         public void Rotate(Shape shape, Direction direction)
         {
-            var angle = Math.PI / 2;
+            var angle = Math.PI / 4;
 
             if (direction == Direction.LEFT)
             {
-                angle *= -1;
+                angle = Math.PI * 5 / 4; // Possibly wrong, double check
             }
 
             foreach (var box in shape.Boxes)
@@ -23,13 +21,22 @@ namespace TetrisAdvanced.Services
                  *  |       ○(x, y)
                  *  |       | \
                  *  |       |  \  radius
-                 *  |       |   \
+                 *  |       |  θ\
                  *  |       ╚----○ (CenterX, CenterY)
                  *  |
                  * -╬----------------
                  *  |
                  * */
                 var radius = Math.Sqrt(Math.Pow(box.X - shape.CenterX, 2) + Math.Pow(box.Y - shape.CenterY, 2));
+
+                if (radius == 0)
+                {
+                    // The box is on the origin, no rotation
+                    continue;
+                }
+
+                // adds θ offset: Cos^-1(Adjcent / Hyp)
+                angle += Math.Acos(Math.Abs(box.X - shape.CenterX) / radius);
 
                 box.X = Convert.ToInt32(shape.CenterX + radius * Math.Cos(angle));
                 box.Y = Convert.ToInt32(shape.CenterY + radius * Math.Sin(angle));
